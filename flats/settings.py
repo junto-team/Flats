da19e7435f37d056874c38d3c2e08db965e5fd4e
Alexey <alexey109@gmail.com>
2017-12-01 16:26:15 +0300

    Add allowed hosts

--- flats/settings.py
+++ flats/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEBUG' in os.environ and os.environ['DEBUG'] == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '159.89.22.6']
 
 
 # Application definition
